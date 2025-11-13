05c4f009ca828251f9ae2e45c73769ca2bb9aba8
rvkong-boop <rvkong@stu.naperville203.org>
2025-11-13 11:48:02 -0600

    i hate this lab and everyone in my aisele

--- ColorLabs17/SheparFaireyLab.java
+++ ColorLabs17/SheparFaireyLab.java
@@ -18,28 +18,138 @@ public class SheparFaireyLab
     {
         
          //opens selfie picture 
-          /**/
+        /*
          String fileName = FileChooser.pickAFile();
-         Picture pictObj = new Picture(fileName);
+         Picture pictObj = new Picture("images/IMG_4739.jpeg");
          pictObj.explore();
+         /**/
          
          //relative path
-         Picture apic = new Picture("images\\beach.jpg");
          //change with selfie picture
-         Picture me = new Picture("images/beach.jpg");
-         Picture me1 = new Picture("images/beach.jpg");
-         Picture me2 = new Picture("images/beach.jpg");
+         Picture me1= new Picture("images/IMG_4739.jpeg");
+         Picture me2 = new Picture("images/IMG_4739.jpeg");
+         Picture me3 = new Picture("images/IMG_4739.jpeg");
+         Picture me4 = new Picture("images/IMG_4739.jpeg");
+         Picture me5 = new Picture("images/IMG_4739.jpeg");
+
+
          
+         Pixel[] pixels1;
+         pixels1 = me1.getPixels();
+     
+        Color darkBlue = new Color(3, 12, 171);
+        Color red = new Color(219, 39, 33);
+        Color lightBlue = new Color(72, 189, 240);
+        Color offWhite = new Color(219, 233, 244);
+
+     
+        /**
+         * greyscale change
+         */
+        for (Pixel pixelObj : pixels1)
+            {
+
+            //get the redvalue
+            int redValue = pixelObj.getBlue();
+            int greenValue = pixelObj.getBlue();
+            int blueValue = pixelObj.getBlue();
+
+            int greyScale = (int)((redValue + greenValue + blueValue)/3);    
+            
+            pixelObj.setRed(greyScale);
+            pixelObj.setGreen(greyScale);
+            pixelObj.setBlue(greyScale);
+
+        }
+        me1.explore();
+        
          /**
           * method 1 change
           * 
           */
          
+        for (Pixel pixelObj : pixels1)
+        {
+            int redValues = pixelObj.getRed();
+            if (redValues < 64){
+                pixelObj.setColor(darkBlue);
+            }
+            else if (redValues < 128){
+                pixelObj.setColor(red);
+            }
+            else if (redValues < 196){
+                pixelObj.setColor(lightBlue);
+            }
+            else{
+                pixelObj.setColor(offWhite);
+            }
+        }
+        me1.explore();
+
          /**
           * method 2 change
           * 
           */
-         
+        
+        int min = 257;
+        int max = 0;
+        int redValues=0, greenValues = 0;
+        
+        for(Pixel pixelObj : pixels1){
+            redValues = pixelObj.getRed();
+            if (redValues < min){
+                min = redValues;
+            }
+            if (redValues > max){
+                max = redValues;
+            }
+        }
+        for (Pixel pixelObj : pixels1)
+        {
+            redValues = pixelObj.getRed();
+            if (redValues < (int)((max-min)/4)){
+                pixelObj.setColor(darkBlue);
+            }
+            else if (redValues < (int)(2*(max-min)/4)){
+                pixelObj.setColor(red);
+            }
+            else if (redValues < (int)(3*(max-min)/4)){
+                pixelObj.setColor(lightBlue);
+            }
+            else{
+                pixelObj.setColor(offWhite);
+            }
+        }
+        me1.explore();
+        
+        for(Pixel pixelObj : pixels1){
+            greenValues = pixelObj.getGreen();
+            if (greenValues < min){
+                min = redValues;
+            }
+            if (greenValues > max){
+                max = redValues;
+            }
+        }
+        for (Pixel pixelObj : pixels1)
+        {
+            greenValues = pixelObj.getGreen();
+            if (greenValues < (int)((max-min)/4)){
+                pixelObj.setColor(darkBlue);
+            }
+            else if (greenValues < (int)(2*(max-min)/4)){
+                pixelObj.setColor(red);
+            }
+            else if (greenValues < (int)(3*(max-min)/4)){
+                pixelObj.setColor(lightBlue);
+            }
+            else{
+                pixelObj.setColor(offWhite);
+            }
+        }
+        me1.explore();
+
+
          /**
           * custom color palette
           */
